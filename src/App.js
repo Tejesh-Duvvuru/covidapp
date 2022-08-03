e53ecee3eceb8da1c731f974311db6ed0218a34a
Tejesh <sduvvuru@information-asset.com>
2022-08-03 06:11:08 +0530

    'mapupdate'

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import StateDetails from './components/StateDetails' 
 import Header from './components/Header'
 import DistrictDetails from './components/DistrictDetails'
+import CovidDataIndianMap from './components/CovidDataIndianMap';
 import { BrowserRouter, Route,  Switch } from 'react-router-dom';
 import './App.css';
 
@@ -12,6 +13,7 @@ function App() {
         <Switch>
         <Route exact path='/' component={StateDetails} />
         <Route exact path='/district/:statename' component={DistrictDetails} />
+        <Route exact path='/mapview' component={CovidDataIndianMap} />
         </Switch>
       </BrowserRouter>
     </div>
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,4 @@
-
+import { Link } from 'react-router-dom'
 import './index.css'
 const Header = () =>{
    
@@ -6,7 +6,12 @@ const Header = () =>{
     return(
         <div className="header-content"> 
             <h1 className='welcome-msg'>Welcome to Covid DataPortal</h1>
-            <h1 className='userName'>Tejesh Duvvuru</h1>
+            <div className='header-right'>
+                <Link to={'/mapview'}>Map</Link>
+                <h1 className='userName'>Tejesh Duvvuru</h1>
+                
+            </div>
+            
         </div>
     ) 
 }
